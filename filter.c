464bc27b7ad35015590cf7e84b3beb1a0f6d9a57
Leonela Leinecker <tu-email@example.com>
2026-03-10 20:01:35 +0100

    powerset

--- filter.c
+++ filter.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filter.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleineck <lleineck@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 16:57:58 by lleineck          #+#    #+#             */
+/*   Updated: 2026/03/10 18:25:21 by lleineck         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -6,9 +18,11 @@
 
 int	ft_strncmp(char *s1, char *s2, int size)
 {
-	int i = 0;
+	int	i;
 
-	while((s1[i] && s2[i]) && i < size)
+	i = 0;
+
+	while ((s1[i] && s2[i]) && i < size)
 	{
 		if (s1[i] != s2[i])
 			return (1);
@@ -19,11 +33,12 @@ int	ft_strncmp(char *s1, char *s2, int size)
 	return (0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	char buf[1000];
-	if (argc != 2 || argv[1][0] == '\0')
-	      return 1;
+	char	buf[1000];
+
+	if (argc != 2 || (argv[1][0] == '\0'))
+		return (1);
 	int i = 0;
 	ssize_t bytes_read;
 	int size = strlen(argv[1]);
---
+++ powerset.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   powerset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lleineck <lleineck@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/10 18:28:32 by lleineck          #+#    #+#             */
+/*   Updated: 2026/03/10 19:52:24 by lleineck         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void	print_subset(int *subset, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (i)
+			printf(" ");
+		printf("%d", subset[i]);
+	}
+	printf("\n");
+}
+
+void	solve(int *set, int n, int target, int index, int *subset, int size, int sum)
+{
+	if (index == n)
+	{
+		if (sum == target)
+			print_subset(subset, size);
+		return;
+	}
+	solve(set, n, target, index + 1, subset, size, sum);
+	subset[size] = set[index];
+	solve(set, n, target, index + 1, subset, size + 1, sum + set[index]);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+		return 0;
+	int target = atoi(argv[1]);
+	int n = argc - 2;
+	int *set = malloc(sizeof(int) * n);
+	int *subset = malloc(sizeof(int) * n);
+	if (!set || !subset)
+		return 1;
+	for (int i = 0; i < n; i++)
+		set[i] = atoi(argv[i + 2]);
+	solve(set, n, target, 0, subset, 0, 0);
+	free(set);
+	free(subset);
+}
